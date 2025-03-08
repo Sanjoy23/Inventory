dc6dfdfea891e5a0d5925ede0f38e12c85114161
Mohamed AlArayedh <ma108@rice.edu>
2025-03-08 09:13:18 -0600

    complete database connection

--- Inventory/Controllers/AuthController.cs
+++ Inventory/Controllers/AuthController.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Inventory.Models;
+using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -13,5 +15,27 @@ namespace Inventory.Controllers
         {
             return View();
         }
+
+        [HttpPost]
+        //public ActionResult Login(string txtUserName, string txtPassword)
+        //{
+        //    if(txtUserName != null)
+        //    {
+        //        ViewBag.UserName = txtUserName;
+        //    }
+        //    return View();
+        //}
+        public ActionResult Login(string txtUserName, string txtPassword)
+        {
+            BasicMember member = new BasicMember();
+            DataTable dt = member.ValidateMemberAsDataTable(txtUserName, txtPassword);
+            if (dt.Rows.Count > 0)
+            {
+                ViewBag.UserName = txtUserName;
+                return Redirect(Url.Action("About", "Home"));
+            }
+            return View();
+        }
+
     }
 }
